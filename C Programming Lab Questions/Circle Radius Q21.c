28dc8ffbea095bb4638a551815bce837c1d68c0a
Bijoy Biswas <156492281+Bijoy0369@users.noreply.github.com>
2024-02-25 16:07:12 +0530

    Area of Circle Radius Q21.c
    Write a program to calculate the area of a circle using using user input for the radius.

--- C Programming Lab Questions/Circle Radius Q21.c
+++ C Programming Lab Questions/Circle Radius Q21.c
@@ -1,1 +1,18 @@
 //Write a program to calculate the area of a circle using using user input for the radius.
+
+#include <stdio.h>  
+  
+int main()   
+{   
+ float Pi= 3.14159, radius,area;
+  
+  
+printf("Enter the radius of the circle: ");  
+scanf("%f", &radius);  
+  
+area = Pi * radius * radius;  
+  
+printf("The area of the circle is: %f", area);  
+  
+return 0;  
+}  
