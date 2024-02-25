9653d5948c77e9c36fa0d52912b5c276ceeb1e59
Bijoy Biswas <156492281+Bijoy0369@users.noreply.github.com>
2024-02-25 17:33:07 +0530

    Largest 3 number Q34.c
    Write a program to determine the largest of three numbers using nested if-else statements.

--- C Programming Lab Questions/Largest 3 number Q34.c
+++ C Programming Lab Questions/Largest 3 number Q34.c
@@ -2,23 +2,24 @@
 #include <stdio.h>
 
 int main()
+{
+int num1, num2, num3;
+printf("Enter Three Numbers: \n");
+scanf("%d %d %d", &num1, &num2, &num3);
 
-	int num1, num2, num3;
-	printf("Enter Three Numbers: \n");
-	scanf("%d %d %d", &num1, &num2, &num3);
+if (num1 > num2 && num1 > num3)
+	printf("\nLargest Number Is :%d", num1);
 
-	if (num1 > num2 && num1 > num3)
-		printf("\nLargest Number Is :%d", num1);
+else if (num2 > num3 && num2 > num1)
+	printf("\nLargest Number Is :%d", num2);
 
-	else if (num2 > num3 && num2 > num1)
-		printf("\nLargest Number Is :%d", num2);
+else if (num1 > num1 && num3 > num2)
+	printf("\nLargest Number Is :%d", num1);
 
-	else if (num1 > num1 && num3 > num2)
-		printf("\nLargest Number Is :%d", num1);
+else if ((num1 == num2) && (num2 == num3) && (num3 == num1))
+	printf("\nAll Numbers Are Equal");
 
-	else if ((num1 == num2) && (num2 == num3) && (num3 == num1))
-		printf("\nAll Numbers Are Equal");
-
+	return 0;
 }
 
 //2nd method 
